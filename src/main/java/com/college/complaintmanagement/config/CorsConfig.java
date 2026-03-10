17c767ae0dd898262c01ec3232c2aa3679488769
Prawin <23suca13@tcarts.in>
2026-03-10 23:55:48 +0530

    Update CORS

---
+++ src/main/java/com/college/complaintmanagement/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.college.complaintmanagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://feedback-frontend-ochre.vercel.app",
+                "https://feedback-frontend-app-gmdwa5a5f9b5fme8.centralindia-01.azurewebsites.net"
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
